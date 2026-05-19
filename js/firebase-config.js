9133e32680f6609a5b23bd622b3f03d9295ee1ba
IkerPlays <ikerana35018santanaiker@gmail.com>
2026-05-19 20:35:35 +0100

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,17 +1,18 @@
 // js/firebase-config.js
-// Configuración de Firebase para ShareIt
 const firebaseConfig = {
-  apiKey: "AIzaSyDKurDI4iIcV_6LnzLkML0E3hM9hqWaMZg",
-  authDomain: "shareit-c1222.firebaseapp.com",
-  databaseURL: "https://shareit-c1222-default-rtdb.firebaseio.com",
-  projectId: "shareit-c1222",
-  storageBucket: "shareit-c1222.firebasestorage.app",
-  messagingSenderId: "466346058481",
-  appId: "1:466346058481:web:733bcea3730cac9d9a3f50"
+    apiKey: "AIzaSyDKurDI4iIcV_6LnzLkML0E3hM9hqWaMZg",
+    authDomain: "shareit-c1222.firebaseapp.com",
+    databaseURL: "https://shareit-c1222-default-rtdb.firebaseio.com",
+    projectId: "shareit-c1222",
+    storageBucket: "shareit-c1222.firebasestorage.app",
+    messagingSenderId: "466346058481",
+    appId: "1:466346058481:web:733bcea3730cac9d9a3f50"
 };
 
-// Inicializar Firebase
-firebase.initializeApp(firebaseConfig);
+// Solo inicializar una vez
+if (!firebase.apps || firebase.apps.length === 0) {
+    firebase.initializeApp(firebaseConfig);
+}
 
 // Referencias globales
 const auth = firebase.auth();
